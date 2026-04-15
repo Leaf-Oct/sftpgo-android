4b1e8aaacda9ac56bbc36c946448d97674c2ee48
leaf oct <leafoct@gmail.com>
2026-04-16 00:24:25 +0800

    Complete advanced setting. Allow user to use other text editor app to edit config file "sftpgo.json".

--- app/src/main/java/cn/leaf/sftpgo/android/fragment/SettingFragment.java
+++ app/src/main/java/cn/leaf/sftpgo/android/fragment/SettingFragment.java
@@ -1,16 +1,22 @@
 package cn.leaf.sftpgo.android.fragment;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import org.json.JSONObject;
+
+import java.io.File;
 
 import cn.leaf.sftpgo.android.R;
 import cn.leaf.sftpgo.android.databinding.FragmentSettingBinding;
@@ -46,6 +52,23 @@ public class SettingFragment extends Fragment {
                     break;
                 case 1:
 //                    requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new SwitchFragment()).commit();
+//                    var sftpgo_json_config_file=;
+                    var file_uri= FileProvider.getUriForFile(requireContext(),"cn.leaf.sftpgo.fileprovider",new File(requireActivity().getExternalFilesDir("conf"),"sftpgo.json"));
+                    var i=new Intent(Intent.ACTION_EDIT);
+//                    i.setDataAndType(file_uri,"application/json");
+                    i.setDataAndType(file_uri, "text/plain");
+                    i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    i.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                    try {
+                        startActivity(i);
+                    } catch (ActivityNotFoundException e){
+                        e.printStackTrace();
+                        Toast.makeText(requireContext(),"外部无可用文本编辑器",Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        Toast.makeText(requireContext(),"打开外部编辑器出错",Toast.LENGTH_SHORT).show();
+                        throw new RuntimeException(e);
+//                        e.printStackTrace();
+                    }
                     break;
                 case 2:
 //                    requireActivity().getSupportFragmentManager().beginTransaction().replace()
