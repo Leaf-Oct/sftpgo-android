20c33cc8f750753a669166f95175e80f0211d224
leaf oct <leafoct@gmail.com>
2026-04-22 00:18:13 +0800

    Add health check. Update theme

--- app/src/main/java/cn/leaf/sftpgo/android/fragment/SwitchFragment.java
+++ app/src/main/java/cn/leaf/sftpgo/android/fragment/SwitchFragment.java
@@ -2,6 +2,7 @@ package cn.leaf.sftpgo.android.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import org.json.JSONException;
@@ -16,9 +18,18 @@ import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.CompletableFuture;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -44,10 +55,12 @@ public class SwitchFragment extends Fragment {
         binding.switchMain.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
                 getActivity().startService(new Intent(getActivity(), SftpgoService.class));
-//                TODO
+                healthCheck();
             } else {
                 getActivity().stopService(new Intent(getActivity(), SftpgoService.class));
                 sftpgo_status.setText("当前状态：关闭");
+                sftpgo_status.setClickable(false);
+                sftpgo_status.setOnClickListener(null);
             }
         });
     }
@@ -62,42 +75,127 @@ public class SwitchFragment extends Fragment {
     public void onStart() {
         super.onStart();
         if (SftpgoService.isRunning) {
-//            TODO
+            healthCheck();
         }
 
     }
 
+
     private void healthCheck() {
         sftpgo_status.setText("检查sftpgo服务状态中");
-        new Thread(()->{
+//        binding.switchMain.setActivated(false);
+        binding.switchMain.setEnabled(false);
+        new Thread(() -> {
+            int response_code = 0;
+            String response_message = null;
+            var sb = new StringBuilder();
+            boolean enable_https = false;
+            List<String> ips=new ArrayList<>();
+            ips.add("127.0.0.1");
+            int http_port = 0;
             try {
                 var http_binding = new JSONObject(new String(Files.readAllBytes(new File(getActivity().getExternalFilesDir("conf"), "sftpgo.json").toPath()), StandardCharsets.UTF_8)).getJSONObject("httpd").getJSONArray("bindings").getJSONObject(0);
-                var http_port = http_binding.getInt("port");
-                var enable_https = http_binding.getBoolean("enable_https");
+                http_port = http_binding.getInt("port");
+                Log.i("http_port", String.valueOf(http_port));
+                enable_https = http_binding.getBoolean("enable_https");
+                Thread.sleep(3000);
                 if (enable_https) {
-                    var url=new URL("https://127.0.0.1:" + http_port+"/healthz");
-                    var https_connection=(HttpsURLConnection)url.openConnection();
+                    var url = new URL("https://127.0.0.1:" + http_port + "/healthz");
+                    var https_connection = (HttpsURLConnection) url.openConnection();
                     https_connection.setRequestMethod("GET");
                     https_connection.connect();
-                    var response_code = https_connection.getResponseCode();
-                    if (response_code == 200) {
-                        getActivity().runOnUiThread(()->{
-
-                        });
-                    } else {
-
-                    }
+                    response_code = https_connection.getResponseCode();
+                    response_message = https_connection.getResponseMessage();
                     https_connection.disconnect();
-                    return;
+                } else {
+                    var url = new URL("http://127.0.0.1:" + http_port + "/healthz");
+                    var http_connection = (HttpURLConnection) url.openConnection();
+                    http_connection.setRequestMethod("GET");
+                    http_connection.connect();
+                    response_code = http_connection.getResponseCode();
+                    response_message = http_connection.getResponseMessage();
+                    http_connection.disconnect();
                 }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
+                Log.i("health", "Response Code: " + response_code);
+                Log.i("health", "Response Message: " + response_message);
+                ips = getAllAddress();
+                for (String ip : ips) {
+                    sb.append(enable_https?"https://":"http://").append(ip).append(":").append(http_port).append("\n");
+                }
+            }
+            catch (Exception e) {
                 e.printStackTrace();
             }
+            finally {
+                int finalResponse_code = response_code;
+                String finalResponse_message = response_message;
+                boolean finalEnable_https = enable_https;
+                List<String> finalIps = ips;
+                int finalHttp_port = http_port;
+                getActivity().runOnUiThread(() -> {
+                    sftpgo_status.setText(((finalResponse_code == 200 && finalResponse_message.equalsIgnoreCase("ok")) ? "运行中" : "异常") + " http" + (finalEnable_https ? "s":"") + "://" + finalIps.get(0) + ":" + finalHttp_port);
+                    sftpgo_status.setClickable(true);
+                    sftpgo_status.setOnClickListener(v -> {
+                        var dialog = new AlertDialog.Builder(getActivity()).setTitle("可用地址").setMessage(sb.toString()).setPositiveButton("ok", null).create();
+                        dialog.show();
+                    });
+                    binding.switchMain.setEnabled(true);
+                });
+                System.gc();
+            }
         }).start();
+    }
 
+    public ArrayList<String> getAllAddress() {
+        var ipv4_address = new ArrayList<String>();
+        var ipv6_address = new ArrayList<String>();
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (true) {
+                NetworkInterface networkInterface;
+                try {
+                    networkInterface = networkInterfaces.nextElement();
+                } catch (NoSuchElementException e) {
+                    break;
+                }
+                if (networkInterface == null) {
+                    break;
+                }
+                Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+                try {
+                    while (true) {
+                        InetAddress inetAddress;
+                        try {
+                            inetAddress = inetAddresses.nextElement();
+                        } catch (NoSuchElementException e) {
+                            break;
+                        }
+                        if (inetAddress == null) {
+                            break;
+                        }
+//                        if (!inetAddress.isLoopbackAddress()) {
+                            if (inetAddress instanceof Inet4Address) {
+                                ipv4_address.add(inetAddress.getHostAddress());
+                            }
+                            if (inetAddress instanceof Inet6Address) {
+                                ipv6_address.add(inetAddress.getHostAddress());
+                            }
+//                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        if (ipv4_address.isEmpty()) {
+//            ipv4_address.add("127.0.0.1");
+//        }
+        ipv4_address.addAll(ipv6_address);
+        ipv6_address.clear();
+        ipv6_address = null;
+        System.gc();
+        return ipv4_address;
     }
 }
